56cb1a0393b7bfb21b1ad9eb727b79d3d39eeb59
mrdata36 <tzdatascientist36@gmail.com>
2025-12-24 10:55:14 +0300

    Render home page using Django templates

--- core/views.py
+++ core/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def home (request):
-    return HttpResponse("Hello Django My First page")
+def home(request):
+    return render(request, 'core/home.html')
